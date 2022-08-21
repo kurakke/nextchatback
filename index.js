143ef5c7f69e29c881887437452190707e97ff12
kiseki-kiseki <kcanggua@gmail.com>
2022-08-21 18:35:30 +0900

    change PORT number

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: ["http://localhost:3000"],
 });
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 io.on("connection", (socket) => {
   console.log("yahharo connected");
-    socket.on("sendYahharo", (data) => {
-        console.log(data);
-        io.emit("receiveYahharo", data);
-    })
+  socket.on("sendYahharo", (data) => {
+    console.log(data);
+    io.emit("receiveYahharo", data);
+  });
   socket.on("disconnect", () => {
     console.log("yahharo disconnected");
   });
